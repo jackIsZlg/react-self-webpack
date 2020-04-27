65d15147db7078de2e6b48cefcd798cec63bd09d
zlg <1015261475@qq.com>
2020-04-27 11:00:06 +0800

    react-up

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, lazy } from 'react';
 import { Route, Switch } from "react-router-dom";
 import loadable from 'loadable-components'
 
 import S from './index.m.less';
 
-const AboutChild = loadable(() => import('@/views/About/AboutChild'))
+const AboutChild = lazy(() => import('@/views/About/AboutChild'))
 
 export default function about () {
     const [count, setCount] = useState(0);
